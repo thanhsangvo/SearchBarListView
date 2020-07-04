7c6f5e04eec962abd167308d1b27f1544d7a81f1
thanhsangvo <pl.thanhsang0710@gmail.com>
2020-07-04 09:20:55 +0700

    Initial Commit

---
+++ SearchBarListView/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SearchBarListView
+//
+//  Created by Võ Thanh Sang on 7/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchBarListView/SearchBarListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarListViewApp.swift
+//  SearchBarListView
+//
+//  Created by Võ Thanh Sang on 7/4/20.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
